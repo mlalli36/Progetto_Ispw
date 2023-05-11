31daa6ec3f371e386cc67074001886a77edf6ed1
pamelalongo <longopamela99@gmail.com>
2023-05-11 19:12:30 +0200

    interfacce search dinamica

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,12 +1,12 @@
 
-h
-8src/main/java/com/example/progetto_ispw/ProfileView.java,5\0\5025fdf1eeb44afc04e9d1614d293bae9f49b294
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+r
+Bsrc/main/java/com/example/progetto_ispw/ProfileRecensioneView.java,6\c\6c56ef9540d9bf3d41c6c35ac6307bfb43cb381d
 f
 6src/main/java/com/example/progetto_ispw/LoginView.java,1\c\1c4ead1dc8b72cde40b780b924e5bfef8f0db1c3
 r
 Bsrc/main/java/com/example/progetto_ispw/NotificaDaClienteView.java,c\0\c06c7b731d8919ded261b372985744de305631ba
-r
-Bsrc/main/java/com/example/progetto_ispw/ProfileRecensioneView.java,6\c\6c56ef9540d9bf3d41c6c35ac6307bfb43cb381d
 e
 5src/main/java/com/example/progetto_ispw/StartApp.java,d\5\d5050a9bb413ecb53f33f9a68a24905418ecb49a
 l
--- src/main/java/com/example/progetto_ispw/SearchDinamicaView.java
+++ src/main/java/com/example/progetto_ispw/SearchDinamicaView.java
@@ -1,8 +1,14 @@
 package com.example.progetto_ispw;
 
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
 
 public class SearchDinamicaView {
     @FXML
@@ -12,6 +18,29 @@ public class SearchDinamicaView {
     @FXML
     public Button home;
 
+    @FXML
+    public Button fillOutForm;
+    @FXML
+    public VBox myVBox;
+
+    //da riga 27 a riga 42 serve per mostrare o non mostrare il box del "fillOutForm"
+    private final BooleanProperty showBox = new SimpleBooleanProperty(false);
+
+
+    public BooleanProperty showBoxProperty() {
+        return showBox;
+    }
+    public boolean isShowBox() {
+        return showBox.get();
+    }
+    public void setShowBox(boolean value) {
+        showBox.set(value);
+    }
+    public void initialize() {
+        myVBox.visibleProperty().bind(showBox);
+        setShowBox(true); //se è true mostra il box, se è false non lo mostra
+    }
+
     public void profileMethod(ActionEvent actionEvent) { //da fare
     }
 
@@ -20,4 +49,12 @@ public class SearchDinamicaView {
 
     public void homeMethod(ActionEvent actionEvent) { //da fare
     }
+
+
 }
+
+
+
+
+
+
