3f891c66314ebf98781437a60a249097db96c0d1
Matteo Lalli <mlalli36@gmail.com>
2023-08-23 16:56:26 +0200

    aggiunta view della notifica

---
+++ src/main/java/com/example/progetto_ispw/notifications/WorkerProfileViewNotifications.java
@@ -0,0 +1,47 @@
+package com.example.progetto_ispw.notifications;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.text.Text;
+
+public class WorkerProfileViewNotifications {
+    @FXML
+    public Text noAppointmentText;
+
+    @FXML
+    public ScrollPane scrollPane;
+    @FXML
+    public Button notificationButton;
+    @FXML
+    public Label emailLabelWorkerProfile;
+    @FXML
+    public Label surnameLabelWorkerProfile;
+    @FXML
+    public Button changeOfWorkingHoursButton;
+    @FXML
+    public Button exitButton;
+    @FXML
+    public Label nameLabelWorkerProfile;
+    @FXML
+    public Button homeButton;
+    @FXML
+    public Button profileButton;
+
+    public void profileMethod(ActionEvent actionEvent) {
+    }
+
+    public void homeMethod(ActionEvent actionEvent) {
+    }
+
+    public void exitMethod(ActionEvent actionEvent) {
+    }
+
+    public void changeOfWorkingHoursMethod(ActionEvent actionEvent) {
+    }
+
+    public void notificationMethod(ActionEvent actionEvent) {
+    }
+}
